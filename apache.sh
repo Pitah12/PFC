361a5e803009483245be000428ffd447b07aa3e1
Piter <pemarconpc@gmail.com>
2022-04-12 01:48:00 +0200

    añadida la opción de copiar un directorio a /var/www

--- apache.sh
+++ apache.sh
@@ -1,12 +1,20 @@
 if [ -f /etc/apache2/apache2.conf ]; then
         #Mira si el proceso esta corriendo y lo inicia si no esta corriendo.
-        if [ $(ps -ef | grep -v grep | grep apache2 | wc -l) -eq 0 ]; then
+        if [ $(ps -ef | grep -v grep | grep apache2 | wc -l) -eq 1 ]; then
             echo "El servidor web esta parado."
-            echo "Desea iniciarlo?"
-            read -p "s/n: " respuesta
-            if [ $respuesta = "s" ]; then
+            read -p "Desea iniciarlo [1] o copiar un directorio a /var/www [2]?" respuesta
+            if [ $respuesta = "1" ]; then
                 /etc/init.d/apache2 start
                 echo "El servidor web se ha iniciado."
+            elif [ $respuesta = "2" ]; then
+                read -p "Introduzca el directorio a copiar (El directorio a copiar tendrá el nombre de html)": directorio
+                if [ -d $directorio ]; then
+                #Copia los archicos del directorio seleccionado a /var/www
+                    cp -r $directorio /var/www
+                    echo "El directorio se ha copiado."
+                else
+                    echo "El directorio no existe."
+                fi
             fi
         #Si proceso esta corriendo, lo detiene.
         else
